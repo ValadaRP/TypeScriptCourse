af7dc435190ac90d8d2cef835304d1758414c7a8
ValadaRP <s37214@s.pwste.edu.pl>
2023-02-20 22:42:56 +0100

    Property decorators

--- FiveTopic/src/app.ts
+++ FiveTopic/src/app.ts
@@ -1,18 +1,58 @@
-function Logger(logString: string){
-    return function(constructor: Function){
-        console.log(logString);
-        console.log(constructor);
-    }
+// function Logger(logString: string){
+//     return function(constructor: Function){
+//         console.log(logString);
+//         console.log(constructor);
+//     }
+// }
+
+// function WithTemplate(template: string, hookId: string){
+//     return function(constructor: any){
+//         console.log('Rendering template');
+//         const hookElement = document.getElementById(hookId);
+//         const p = new constructor();
+//         if(hookElement){
+//             hookElement.innerHTML = template;
+//             hookElement.querySelector('h1')!.textContent = p.name;
+//         }
+//     }
+// }
+
+// @Logger("Test")
+// @WithTemplate('<h1>My person Object</h1>', 'app')
+// class Person{
+//     name = 'Max';
+
+//     constructor(){
+//         console.log('creating person object...');
+//     }
+// }
+
+// const pers = new Person();
+// console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol){
+    console.log('Property dercorator!');
+    console.log(target, propertyName);
 }
 
-@Logger("Test")
-class Person{
-    name = 'Max';
+class Product {
+    @Log
+    title: string;
+    private _price: number;
 
-    constructor(){
-        console.log('creating person object...');
+    set price(val:number){
+        if(val>0){
+            this.price = val;
+        }else{
+            throw Error('Invalid price - should be positive!');
+        }
+    }
+
+    constructor(t: string, p: number){
+        this.title = t;
+        this._price = p;
+    }
+    getPriceWtihTax(tax:number){
+        return this._price * (1 + tax);
     }
 }
-
-const pers = new Person();
-console.log(pers);
