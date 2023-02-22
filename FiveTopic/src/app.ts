1b71dd487071595ae7fe4a811cb75f5ce5f74524
ValadaRP <s37214@s.pwste.edu.pl>
2023-02-22 13:50:07 +0100

    Autobind with decorators

--- FiveTopic/src/app.ts
+++ FiveTopic/src/app.ts
@@ -6,13 +6,19 @@
 // }
 
 // function WithTemplate(template: string, hookId: string){
-//     return function(constructor: any){
-//         console.log('Rendering template');
-//         const hookElement = document.getElementById(hookId);
-//         const p = new constructor();
-//         if(hookElement){
-//             hookElement.innerHTML = template;
-//             hookElement.querySelector('h1')!.textContent = p.name;
+//     return function<T extend {new(...args: any[]) : {name: string} }>(originalConstructor: T){
+        
+//         return class extend originalConstructor {
+//             constructor(..._: any[]) {
+//                 super();
+//                 console.log('Rendering template');
+//                 const hookElement = document.getElementById(hookId);
+//                 const p = new originalConstructor();
+//                 if(hookElement){
+//                     hookElement.innerHTML = template;
+//                     hookElement.querySelector('h1')!.textContent = this.name;
+//                 }
+//             }
 //         }
 //     }
 // }
@@ -35,11 +41,33 @@ function Log(target: any, propertyName: string | Symbol){
     console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor){
+    console.log('Accesor decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor){
+    console.log('Method decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number){
+    console.log('Parameter decorator!');
+    console.log(target);
+    console.log(name);
+    console.log(position);
+}
+
 class Product {
     @Log
     title: string;
     private _price: number;
 
+    @Log2
     set price(val:number){
         if(val>0){
             this.price = val;
@@ -52,7 +80,35 @@ class Product {
         this.title = t;
         this._price = p;
     }
-    getPriceWtihTax(tax:number){
+    @Log3
+    getPriceWtihTax(@Log4 tax:number){
         return this._price * (1 + tax);
     }
 }
+
+function Autobind(_: any, _2: string | Symbol, descriptor:PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+            const boundFn = originalMethod.bind(this);
+            return boundFn;
+        }
+    };
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!';
+
+    @Autobind
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
