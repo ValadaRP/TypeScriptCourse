1f33d77ff581b81bc36581e09f7ecd97ddb76af2
ValadaRP <s37214@s.pwste.edu.pl>
2023-03-14 23:41:13 +0100

    class-transformer and class-validator

--- 3rd Party Libraries/src/app.ts
+++ 3rd Party Libraries/src/app.ts
@@ -1,3 +1,25 @@
-import _ from 'lodash';
+import { Product } from "./product.model";
+import 'reflect-metadata';
+import { plainToInstance} from 'class-transformer';
+import {validate} from 'class-validator'
 
-console.log(_.shuffle([1,2,3]));
+const products = [{title: 'A carpet', price: 29.99},{title: 'A book', price: 23.99}];
+
+// const p1 = new Product("Book", 14);
+
+const loadedProducts = plainToInstance(Product, products);
+
+for(const prod of loadedProducts){
+    console.log(prod.getInformation());
+}
+
+const newProd = new Product('', -244);
+validate(newProd).then(errors => {
+    if(errors.length > 0){
+
+        console.log(errors);
+    }else{
+        console.log(newProd.getInformation());
+    }
+});
+// console.log(p1.getInformation());
